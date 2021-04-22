ebe5bf9ce4ddf360472fb407ef9d30fde36e8dd3
abhikedia <iamabhikedia74@gmail.com>
2021-04-22 18:33:11 +0530

    Progress in dashboard design

--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import { Button } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Avatar, Button } from '@material-ui/core';
 import ApartmentIcon from '@material-ui/icons/Apartment';
 import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
 import AssessmentIcon from '@material-ui/icons/Assessment';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import HistoryIcon from '@material-ui/icons/History';
+import Background from '../../assets/images/avatar.png';
 import TrackChangesIcon from '@material-ui/icons/TrackChanges';
 import './index.scss';
 
 export default function Dashboard() {
+    const [name, setName] = useState('Abhishek Kedia');
+    const [blood, setBlood] = useState('');
+    const [age, setAge] = useState('22');
+    const [height, setHeight] = useState('180');
+    const [weight, setWeight] = useState('70');
+    const [avatar, setAvatar] = useState(Background);
+
     return (
         <div className="dashboard-main">
             <div className="dashboard-sidepane-left">
@@ -46,7 +54,26 @@ export default function Dashboard() {
             <div className="dashboard-mainportal">
                 <div className="dashboard-user">
                     <div className="dashboard-user-profile">
-                        abcd
+                        <Avatar src={avatar} className="dashboard-avatar" />
+                        <div className="dashboard-name">{name}</div>
+                        <hr className="dashboard-user-profile-vertical" />
+                        <hr className="dashboard-user-profile-horizontal" />
+                        <div>
+                            <div>{age}</div>
+                            <div>Age</div>
+                        </div>
+                        <div>
+                            <div>{height}</div>
+                            <div>Height</div>
+                        </div>
+                        <div>
+                            <div>{weight}</div>
+                            <div>Weight</div>
+                        </div>
+                        <div>
+                            <div>{blood}</div>
+                            <div>Blood Group</div>
+                        </div>
                     </div>
                     <div>
 
