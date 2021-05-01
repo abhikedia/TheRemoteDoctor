04479df39099ea4cb5b8fe631b2f72796e116313
abhikedia <iamabhikedia74@gmail.com>
2021-05-01 13:26:36 +0530

    Updating Dashboard

--- database.sql
+++ database.sql
@@ -1,12 +1,52 @@
 CREATE TABLE `Patients` (
 	`patientid` INT NOT NULL,
-	`email` varchar(255) NOT NULL,
+	`email` varchar(255) NOT NULL UNIQUE,
 	`name` varchar(255) NOT NULL,
 	`gender` varchar(1) NOT NULL,
 	`password` varchar(255) NOT NULL,
 	`phone` varchar(255) NOT NULL,
 	`avatar` varchar(255) NOT NULL,
 	`dob` DATE NOT NULL,
+	`height` INT,
+	`weight` INT,
+	`blood` varchar(3),
 	PRIMARY KEY (`patientid`)
 );
+
+CREATE TABLE `Appointments` (
+	`appointment_number` INT NOT NULL,
+	`patient_id` INT NOT NULL,
+	`date` DATE NOT NULL,
+	`time` TIME NOT NULL,
+	`doctor_id` INT NOT NULL,
+	PRIMARY KEY (`appointment_number`)
+);
+
+CREATE TABLE `Doctors` (
+	`doctorid` INT NOT NULL,
+	`name` varchar(255) NOT NULL DEFAULT '1',
+	`hospital` varchar(255) DEFAULT '1',
+	`fees` INT,
+	`degree` varchar(255) NOT NULL,
+	`timings` varchar(255) NOT NULL,
+	`specialization` varchar(255) NOT NULL,
+	`state` varchar(255) NOT NULL,
+	`city` varchar(255) NOT NULL,
+	`upi` varchar(255),
+	`phone` varchar(255),
+	`dept` INT NOT NULL,
+	PRIMARY KEY (`doctorid`)
+);
+
+CREATE TABLE `Department` (
+	`deptid` INT NOT NULL,
+	`name` varchar(255) NOT NULL,
+	PRIMARY KEY (`deptid`)
+);
+
+ALTER TABLE `Appointments` ADD CONSTRAINT `Appointments_fk0` FOREIGN KEY (`patient_id`) REFERENCES `Patients`(`patientid`);
+
+ALTER TABLE `Appointments` ADD CONSTRAINT `Appointments_fk1` FOREIGN KEY (`doctor_id`) REFERENCES `Doctors`(`doctorid`);
+
+ALTER TABLE `Doctors` ADD CONSTRAINT `Doctors_fk0` FOREIGN KEY (`dept`) REFERENCES `Department`(`deptid`);
 
--- src/Components/Dashboard/index.js
+++ src/Components/Dashboard/index.js
@@ -1,91 +1,119 @@
-import React, { useState } from 'react';
-import { Avatar, Button } from '@material-ui/core';
-import ApartmentIcon from '@material-ui/icons/Apartment';
-import AllInclusiveIcon from '@material-ui/icons/AllInclusive';
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import HistoryIcon from '@material-ui/icons/History';
-import Background from '../../assets/images/avatar.png';
+import React, { useState } from "react";
+import { Avatar, Button } from "@material-ui/core";
+import ApartmentIcon from "@material-ui/icons/Apartment";
+import AllInclusiveIcon from "@material-ui/icons/AllInclusive";
+import Grid from "@material-ui/core/Grid";
+import Modal from '@material-ui/core/Modal';
+import AssessmentIcon from "@material-ui/icons/Assessment";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import TrackChangesIcon from '@material-ui/icons/TrackChanges';
-import './index.scss';
+import HistoryIcon from "@material-ui/icons/History";
+import Background from "../../assets/images/avatar.png";
+import "./index.scss";
 
 export default function Dashboard() {
-    const [name, setName] = useState('Abhishek Kedia');
-    const [blood, setBlood] = useState('');
-    const [age, setAge] = useState('22');
-    const [height, setHeight] = useState('180');
-    const [weight, setWeight] = useState('70');
-    const [avatar, setAvatar] = useState(Background);
+  const [name, setName] = useState("Abhishek Kedia");
+  const [blood, setBlood] = useState("B+");
+  const [age, setAge] = useState("22");
+  const [height, setHeight] = useState("180");
+  const [weight, setWeight] = useState("70");
+  const [avatar, setAvatar] = useState(Background);
 
-    return (
-        <div className="dashboard-main">
-            <div className="dashboard-sidepane-left">
-                <div className="dashboard-header">
-                    The Remote Doctor
-                </div>
-                <hr className="dashboard-line" />
-                <div className="dashboard-options">
-                    <div>
-                        <AllInclusiveIcon fontSize="large" />
-                        <Button className="dashboard-buttons">Overview</Button>
-                    </div>
-                    <div>
-                        <ApartmentIcon />
-                        <Button className="dashboard-buttons">New Appointment</Button>
-                    </div>
-                    <div>
-                        <TrackChangesIcon />
-                        <Button className="dashboard-buttons">Track Appointment</Button>
-                    </div>
-                    <div>
-                        <AssessmentIcon />
-                        <Button className="dashboard-buttons">Lab Reports</Button>
-                    </div>
-                    <div>
-                        <HistoryIcon />
-                        <Button className="dashboard-buttons">History</Button>
-                    </div>
-                    <div className="dashboard-logout">
-                        <ExitToAppIcon />
-                        <Button className="dashboard-buttons">LogOut</Button>
-                    </div>
-                </div>
-            </div>
-            <div className="dashboard-mainportal">
-                <div className="dashboard-user">
-                    <div className="dashboard-user-profile">
-                        <Avatar src={avatar} className="dashboard-avatar" />
-                        <div className="dashboard-name">{name}</div>
-                        <hr className="dashboard-user-profile-vertical" />
-                        <hr className="dashboard-user-profile-horizontal" />
-                        <div>
-                            <div>{age}</div>
-                            <div>Age</div>
-                        </div>
-                        <div>
-                            <div>{height}</div>
-                            <div>Height</div>
-                        </div>
-                        <div>
-                            <div>{weight}</div>
-                            <div>Weight</div>
-                        </div>
-                        <div>
-                            <div>{blood}</div>
-                            <div>Blood Group</div>
-                        </div>
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
-                <div className="dashboard-chart">
-                    s
-                </div>
-            </div>
-            <div className="dashboard-sidepane-right">
-                pp
-            </div>
+  return (
+    <div id="dashboard-main">
+      <div id="dashboard-sidepane-left">
+        <div className="dashboard-header">
+          <u>The Remote Doctor</u>
         </div>
-    );
+
+        <div className="dashboard-options">
+          <Grid container alignItems="center">
+            <Grid item>
+              <AllInclusiveIcon />
+            </Grid>
+            <Grid item>
+              <Button className="dashboard-buttons">Overview</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container alignItems="center">
+            <Grid item>
+              <ApartmentIcon />
+            </Grid>
+            <Grid item>
+              <Button className="dashboard-buttons">New Appointment</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container alignItems="center">
+            <Grid item>
+              <AssessmentIcon />
+            </Grid>
+            <Grid item>
+              <Button className="dashboard-buttons">Track Appointment</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container alignItems="center">
+            <Grid item>
+              <TrackChangesIcon />
+            </Grid>
+            <Grid item>
+              <Button className="dashboard-buttons">Reports</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container alignItems="center">
+            <Grid item>
+              <HistoryIcon />
+            </Grid>
+            <Grid item>
+              <Button className="dashboard-buttons">History</Button>
+            </Grid>
+          </Grid>
+        </div>
+
+        <Grid className="dashboard-logout" container alignItems="center">
+          <Grid item>
+            <ExitToAppIcon />
+          </Grid>
+          <Grid item>
+            <Button className="dashboard-buttons">LogOut</Button>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div id="dashboard-mainportal">
+        <div className="dashboard-user">
+          <div id="dashboard-user-profile">
+            <div className="dashboard-avatar">
+              <Avatar src={avatar} />
+            </div>
+            <div className="dashboard-name">{name}</div>
+            <div className="profile-data">
+              <div>
+                <span className="data-head">Age: </span>
+                <span className="data-ans">{age}</span>
+              </div>
+              <div>
+                <span className="data-head">Height: </span>
+                <span className="data-ans">{height}</span>
+              </div>
+              <div>
+                <span className="data-head">Weight: </span>
+                <span className="data-ans">{weight}</span>
+              </div>
+              <div>
+                <span className="data-head">Blood Group: </span>
+                <span className="data-ans">{blood}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="dashboard-chart">s</div>
+      </div>
+
+      <div id="dashboard-sidepane-right">pp</div>
+    </div>
+  );
 }
